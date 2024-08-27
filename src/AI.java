639635aa7ab1cb883a5d7bdd7f9403b99be7c884
Predrag777 <predragbrajovicSS@gmail.com>
2024-08-27 10:14:44 +0200

    SS

--- src/AI.java
+++ src/AI.java
@@ -4,8 +4,10 @@ public class AI {
 	
 	private boolean move;
 	private boolean attack;
-	private boolean defend;
 	private int health;
+	private boolean jump;
+	private boolean block;
+	private boolean punched;
 	private int x,y;
 	private int a,b;
 	
@@ -13,34 +15,25 @@ public class AI {
 	boolean retreatRight=false;
 	boolean retreatLeft=false;
 	
-	public AI(int health, int x, int y, int a, int b, boolean move, boolean attack, boolean defend) {
+	public AI(int health, int x, int y, int a, int b, boolean attack) {
 		this.health=health;
-		this.move=move;
+		this.move=move();
 		this.attack=attack;
-		this.defend=defend;
+		
 		this.x=x;
 		this.y=y;
 		
 		this.b=b;
-		this.a=a;
-		
+		this.a=a;		
 	}
 	
-	public int move() {// 0 stoj, 1 napred, 2 nazad, 3 nazad desno, 4 nazad levo
-		if(!retreatRight && x<a && a>=x+borders) {
-			retreatRight=false;
-			return 1;
-		}else if(x<a && a<=x+borders){
-			retreatRight=true;;
-			return 3;
-		}else if(x>a && a<=x-borders) {
-			return 2;
-		}else if(x>a && a>=x-borders){
-			return 4;
-		}
-		
-		
-		return 0;
+	public boolean move() {
+		System.out.println(attack);
+		return (!attack && !isInAttack(this.a));
+	}
+	
+	public boolean isInAttack(int a) {
+		return a<=x+200;
 	}
 	
 	
--- src/Menu.java
+++ src/Menu.java
@@ -1,153 +1,160 @@
-import java.awt.Component;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.PrintStream;
 import java.util.Random;
-import java.awt.event.ActionEvent;
 
 public class Menu {
 
-	private JFrame frame;
-	private JTextField txt;
+    private JFrame frame;
+    private JLabel txt;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Menu window = new Menu();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    private int count;
+    private Fighter[] fighters;
+    private Fighter fighter1;
+    private Fighter fighter2;
 
-	/**
-	 * Create the application.
-	 */
-	private int count;
-	private Fighter fighters[];
-	private Fighter fighter1;
-	private Fighter fighter2;
-	
-	private boolean log;
-	public Menu() {
-		Test t=new Test();
-		t.test();
-		this.count=0;
-		this.fighters=t.getFighters();
-		this.log=false;
-		initialize();
-	}
+    private boolean log;
 
-	public void setFighter1(Fighter fighter1) {
-		this.fighter1 = fighter1;
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Menu window = new Menu();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	public void setFighter2(Fighter fighter2) {
-		this.fighter2 = fighter2;
-	}
+    public Menu() {
+        Test t = new Test();
+        t.test();
+        this.count = 0;
+        this.fighters = t.getFighters();
+        this.log = false;
+        initialize();
+    }
 
-	public Fighter getFighter1() {
-		return fighter1;
-	}
+    public void setFighter1(Fighter fighter1) {
+        this.fighter1 = fighter1;
+    }
 
-	public Fighter getFighter2() {
-		return fighter2;
-	}
+    public void setFighter2(Fighter fighter2) {
+        this.fighter2 = fighter2;
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 708, 473);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JLabel imgLbl = new JLabel("");
-		imgLbl.setBounds(39, 22, 273, 275);
-		frame.getContentPane().add(imgLbl);
-		
-		JLabel lblName = new JLabel("Name: ");
-		lblName.setBounds(39, 322, 70, 15);
-		frame.getContentPane().add(lblName);
-		
-		txt = new JTextField();
-		txt.setBounds(98, 320, 134, 19);
-		frame.getContentPane().add(txt);
-		txt.setColumns(10);
-		
-		
-		JLabel lblImg2 = new JLabel("");
-		lblImg2.setBounds(324, 22, 273, 275);
-		frame.getContentPane().add(lblImg2);
-		
-		
-		JButton btnNext = new JButton("Next");
-		btnNext.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if(count>=fighters.length || fighters[count]==null)
-					count=0;
-				fighter1=fighters[count];
-				//System.out.println(count);
-				txt.setText(fighters[count].getName());
-				String url="images/";
-				ImageIcon img=new ImageIcon(url+fighters[count].getImg());
-				imgLbl.setIcon(img);
-				count++;
-			}
-		});
-		btnNext.setBounds(39, 403, 117, 25);
-		frame.getContentPane().add(btnNext);
-		
-		JButton btnSubmit = new JButton("Submit");
-		btnSubmit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			//	setFighter1(fighters[count]);
-				Random rand=new Random();
-				int c=0;
-				
-				c=rand.nextInt(fighters.length);
-				setFighter2(fighters[c]);
-				String url="images/";
-				ImageIcon img=new ImageIcon(url+fighters[c].getImg());
-				lblImg2.setIcon(img);
-				fighter2=fighters[2];
-		//		System.out.println(fighter1.getName()+" VS "+fighter2.getName());
-				File file=new File("Borbe.txt");
-				try {
-					PrintStream ps=new PrintStream(file);
-					ps.println(fighter1.getName()+" VS "+fighter2.getName());
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				Crtaj a=new Crtaj(fighter1, fighter2);
-				JFrame p=new JFrame();
-				p.setSize(1000,1000);
-				p.setVisible(true);
-				p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		        p.add(a);
-			}
-		});
-		btnSubmit.setBounds(591, 403, 95, 25);
-		frame.getContentPane().add(btnSubmit);
-		
-		
-	}
+    public Fighter getFighter1() {
+        return fighter1;
+    }
+
+    public Fighter getFighter2() {
+        return fighter2;
+    }
+
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 708, 473);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        // Kreiranje panela za pozadinu
+        BackgroundPanel backgroundPanel = new BackgroundPanel("background/stage1.jpg"); // Proverite da li je putanja ispravna
+        backgroundPanel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+        frame.getContentPane().add(backgroundPanel);
+        backgroundPanel.setLayout(null);
+
+        JLabel imgLbl = new JLabel("");
+        imgLbl.setBounds(39, 22, 273, 275);
+        backgroundPanel.add(imgLbl);
+
+        JLabel lblName = new JLabel("Name: ");
+        lblName.setBounds(39, 322, 70, 15);
+        lblName.setForeground(Color.WHITE);
+        backgroundPanel.add(lblName);
+
+        txt = new JLabel();
+        txt.setBounds(98, 320, 134, 19);
+        txt.setForeground(Color.WHITE);
+        backgroundPanel.add(txt);
+        
+
+        JLabel lblImg2 = new JLabel("");
+        lblImg2.setBounds(324, 22, 273, 275);
+        backgroundPanel.add(lblImg2);
+
+        JButton btnNext = new JButton("Next");
+        btnNext.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                if (count >= fighters.length || fighters[count] == null)
+                    count = 0;
+                fighter1 = fighters[count];
+                txt.setText(fighters[count].getName());
+                String url = "images/";
+                ImageIcon img = new ImageIcon(url + fighters[count].getImg());
+                imgLbl.setIcon(img);
+                count++;
+            }
+        });
+        btnNext.setBounds(39, 403, 117, 25);
+        backgroundPanel.add(btnNext);
+
+        JButton btnSubmit = new JButton("Submit");
+        btnSubmit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                Random rand = new Random();
+                int c = 0;
+
+                c = rand.nextInt(fighters.length);
+                setFighter2(fighters[c]);
+                String url = "images/";
+                ImageIcon img = new ImageIcon(url + fighters[c].getImg());
+                lblImg2.setIcon(img);
+                fighter2 = fighters[2];
+                File file = new File("Borbe.txt");
+                try {
+                    PrintStream ps = new PrintStream(file);
+                    ps.println(fighter1.getName() + " VS " + fighter2.getName());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Crtaj a = new Crtaj(fighter1, fighter2);
+                JFrame p = new JFrame();
+                p.setSize(1000, 1000);
+                p.setVisible(true);
+                p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                p.add(a);
+            }
+        });
+        btnSubmit.setBounds(591, 403, 95, 25);
+        backgroundPanel.add(btnSubmit);
+    }
+    
+    class BackgroundPanel extends JPanel {
+        private Image backgroundImage;
+
+        public BackgroundPanel(String imagePath) {
+            try {
+                backgroundImage = new ImageIcon(imagePath).getImage();
+                if (backgroundImage == null) {
+                    System.err.println("Slika nije pronađena: " + imagePath);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (backgroundImage != null) {
+                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+            }
+        }
+    }
 }
